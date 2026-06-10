ac0d32b9ef79658dd57c1518795b16efdeb3ab7a
Egor Polyakov <egor.p@nsysgroup.com>
2026-06-10 18:20:40 +0300

    Add player scene with movement and basic level

---
+++ player.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+@export var speed := 300.0
+@export var jump_velocity := -400.0
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = jump_velocity
+
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+
+	move_and_slide()
